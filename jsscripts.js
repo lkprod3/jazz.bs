7bc78d94243530a222ee34434a3d07d961bdb7b9
lkprod3 <lkprod2@gmail.com>
2018-04-17 23:33:28 -0300

    Logo text now working as it should

--- jsscripts.js
+++ jsscripts.js
@@ -2,7 +2,7 @@
 
    $(document).ready(function(){
   // Add smooth scrolling to all links
-  $(".navbar1 a").on('click', function(event) {
+  $(".navbar1 a, .nodeco").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
